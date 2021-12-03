f94a973f3c41345e6d199dfd13ca3408f0284ec4
José Duque <duquejo01@gmail.com>
2021-12-02 22:41:23 -0500

    Not found public routes fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dbConnection();
 
 app.use( cors() );
 
-app.use( express.static( 'public' ) ); // Public Path
+app.use( express.static( path.resolve( __dirname, 'public' ))); // Public Path
 
 app.use( express.json() ); // Parse JSON contents
 
@@ -24,7 +24,7 @@ app.use( '/api/events', require( './routes/events' ) ); // Events routes
 
 // Default route
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile( path.resolve( __dirname, 'public', 'index.html' ));
 });
 
 app.listen( port, () => {
